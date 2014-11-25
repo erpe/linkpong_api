4f9f773883b162b43887b46cf7b3765182a50663
Rene Paulokat <rene@so36.net>
2014-11-25 21:35:27 +0100

    use gorilla/mux

---
+++ linkpong_api_server.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+
+	// stores collection
+	stores := r.Path("/stores").Subrouter()
+	stores.Methods("GET").HandlerFunc(StoresIndexHandler)
+	stores.Methods("POST").HandlerFunc(StoresCreateHandler)
+
+	// Store singular
+	store := r.PathPrefix("/store/{id}").Subrouter()
+	store.Methods("GET").HandlerFunc(StoreShowHandler)
+	store.Methods("PUT", "POST").HandlerFunc(StoreUpdateHandler)
+	store.Methods("DELETE").HandlerFunc(StoreDeleteHandler)
+
+	// links collection
+	links := r.Path("stores/{store_id}/links").Subrouter()
+	links.Methods("GET").HandlerFunc(StoreLinksIndexHandler)
+	links.Methods("POST").HandlerFunc(StoreLinksCreateHandler)
+
+	// links singular
+	link := r.Path("/store/{store_id}/links/{id}").Subrouter()
+	link.Methods("GET").HandlerFunc(StoreLinkShowHandler)
+	link.Methods("PUT", "POST").HandlerFunc(StoreLinkUpdateHandler)
+	link.Methods("DELETE").HandlerFunc(StoreLinkDeleteHandler)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+	var buffer bytes.Buffer
+	buffer.WriteString("GET /\n")
+	buffer.WriteString("GET|POST /stores/ - index|creates store\n")
+	buffer.WriteString("GET|POST|PUT|DELETE /stores/{id} - reads|updates|deletes store\n")
+	buffer.WriteString("GET|POST /stores/{id}/links - index|creates link\n")
+	buffer.WriteString("GET|POST|PUT|DELETE /stores/{store_id}/links/{id} - reads|updates|deletes link\n")
+	rw.Write([]byte(buffer.String()))
+}
+
+func StoresIndexHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "GET /stores - stores index"
+	rw.Write([]byte(text))
+}
+
+func StoresCreateHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "POST /stores - create store"
+	rw.Write([]byte(text))
+}
+
+func StoreShowHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "GET /stores/{id} - show store"
+	rw.Write([]byte(text))
+}
+
+func StoreUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "POST | PUT /stores/{id} - updates store"
+	rw.Write([]byte(text))
+}
+
+func StoreDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "DELETE /stores/{id} - deletes store"
+	rw.Write([]byte(text))
+}
+
+func StoreLinksIndexHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "GET /stores/{store_id}/links - links index"
+	rw.Write([]byte(text))
+}
+
+func StoreLinksCreateHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "POST /stores/{store_id}/links - create link"
+	rw.Write([]byte(text))
+}
+
+func StoreLinkShowHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "GET /stores/{store_id}/links/{id} - show link"
+	rw.Write([]byte(text))
+}
+
+func StoreLinkUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "POST | PUT /stores/{store_id}/links/{id} - update link"
+	rw.Write([]byte(text))
+}
+
+func StoreLinkDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	text := "DELETE /stores/{store_id}/links/{id} - delete link"
+	rw.Write([]byte(text))
+}
